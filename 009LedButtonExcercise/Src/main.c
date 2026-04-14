fa5182235296074a14dcebd90caf26b6e4d11fd9
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2026-04-14 16:03:28 +0300

    Move define registers to global variables

--- 009LedButtonExcercise/Src/main.c
+++ 009LedButtonExcercise/Src/main.c
@@ -3,13 +3,21 @@
 
 #include <stdint.h>
 
-int main(void) {
-	uint32_t rcc_base = 0x40023800UL;
-	volatile uint32_t *ahb1_enr = (volatile uint32_t*) (rcc_base + 0x30);
+uint32_t rcc_base = 0x40023800UL;
+uint32_t gpioc_base = 0x40020800UL;
 
-	uint32_t gpioc_base = 0x40020800UL;
-	volatile uint32_t *gpioc_moder = (volatile uint32_t*) (gpioc_base + 0x00);
-	volatile uint32_t *gpioc_bsrr = (volatile uint32_t*) (gpioc_base + 0x18);
+volatile uint32_t *ahb1_enr;
+
+volatile uint32_t *gpioc_moder;
+volatile uint32_t *gpioc_bsrr;
+
+void delay(volatile uint32_t count);
+
+int main(void) {
+	ahb1_enr = (volatile uint32_t*) (rcc_base + 0x30);
+
+	gpioc_moder = (volatile uint32_t*) (gpioc_base + 0x00);
+	gpioc_bsrr = (volatile uint32_t*) (gpioc_base + 0x18);
 
 	// Enable AHB1 (Advanced High-performance bus) Port C
 	*ahb1_enr |= (1 << 2);
@@ -21,5 +29,32 @@ int main(void) {
 	// the LED is inverted on the board so add +16bits
 	*gpioc_bsrr = (1 << (13 + 16));
 
-	for (;;);
+	// Just toggle the LED
+	// wait 1s, turn off, wait .5s turn off, wait .5s turn off
+	delay(1000000);
+
+	*gpioc_bsrr = (1 << 13);
+	delay(500000);
+
+	*gpioc_bsrr = (1 << (13 + 16));
+	delay(500000);
+
+	*gpioc_bsrr = (1 << 13);
+	delay(500000);
+
+	*gpioc_bsrr = (1 << (13 + 16));
+	delay(500000);
+
+	*gpioc_bsrr = (1 << 13);
+	delay(500000);
+
+	for (;;) {
+
+	}
+}
+
+void delay(volatile uint32_t count) {
+	for (int i = 0; i < count; i++) {
+		__asm("nop");
+	}
 }
