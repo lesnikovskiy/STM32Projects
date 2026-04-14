fbde483838861ad939b859cb534110c4581a18e1
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2026-04-14 16:08:15 +0300

    Add some code readability extending the REG operations to functions

--- 009LedButtonExcercise/Src/main.c
+++ 009LedButtonExcercise/Src/main.c
@@ -12,6 +12,9 @@ volatile uint32_t *gpioc_moder;
 volatile uint32_t *gpioc_bsrr;
 
 void delay(volatile uint32_t count);
+void led_on(void);
+void led_off(void);
+void startup_led_blink(void);
 
 int main(void) {
 	ahb1_enr = (volatile uint32_t*) (rcc_base + 0x30);
@@ -25,31 +28,10 @@ int main(void) {
 	*gpioc_moder &= ~(3 << 26); // clear bits 26-27
 	*gpioc_moder |= (1 << 26); // set output mode to bit 26 (01)
 
-	// Now turn on bit with BSRR
-	// the LED is inverted on the board so add +16bits
-	*gpioc_bsrr = (1 << (13 + 16));
-
-	// Just toggle the LED
-	// wait 1s, turn off, wait .5s turn off, wait .5s turn off
-	delay(1000000);
-
-	*gpioc_bsrr = (1 << 13);
-	delay(500000);
-
-	*gpioc_bsrr = (1 << (13 + 16));
-	delay(500000);
-
-	*gpioc_bsrr = (1 << 13);
-	delay(500000);
-
-	*gpioc_bsrr = (1 << (13 + 16));
-	delay(500000);
-
-	*gpioc_bsrr = (1 << 13);
-	delay(500000);
+	startup_led_blink();
 
 	for (;;) {
-
+		__asm("nop");
 	}
 }
 
@@ -57,4 +39,36 @@ void delay(volatile uint32_t count) {
 	for (int i = 0; i < count; i++) {
 		__asm("nop");
 	}
+}
+void led_on(void) {
+	*gpioc_bsrr = (1 << (13 + 16));
+}
+
+void led_off(void) {
+	*gpioc_bsrr = (1 << 13);
+}
+
+void startup_led_blink(void) {
+	// Now turn on bit with BSRR
+	// the LED is inverted on the board so add +16bits
+	led_on();
+
+	// Just toggle the LED
+	// wait 1s, turn off, wait .5s turn off, wait .5s turn off
+	delay(1000000);
+
+	led_off();
+	delay(500000);
+
+	led_on();
+	delay(500000);
+
+	led_off();
+	delay(500000);
+
+	led_on();
+	delay(500000);
+
+	led_off();
+	delay(500000);
 }
