630ab4375dcefafa44122c4d0f67d0e2fa4d47c5
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2026-04-15 13:39:42 +0300

    Add interrupts to set LED blink mode

--- 009LedButtonExcercise/Src/main.c
+++ 009LedButtonExcercise/Src/main.c
@@ -9,11 +9,13 @@ const uint32_t GPIOC_BASE = 0x40020800UL;
 const uint32_t USART2_BASE = 0x40004400UL;
 const uint32_t TIM2_BASE = 0x40000000UL;
 const uint32_t SYSCFG_BASE = 0x40013800UL;
+const uint32_t EXTI_BASE = 0x40013C00UL;
 
 volatile uint32_t *const DBGMCU_CR = (volatile uint32_t*) 0xE0042004UL;
 
 volatile uint32_t *const AHB1_ENR = (volatile uint32_t*) (RCC_BASE + 0x30UL);
 volatile uint32_t *const APB1_ENR = (volatile uint32_t*) (RCC_BASE + 0x40UL);
+volatile uint32_t *const APB2_ENR = (volatile uint32_t*) (RCC_BASE + 0x44UL);
 
 volatile uint32_t *const GPIOA_MODER = (volatile uint32_t*) (GPIOA_BASE + 0x00UL);
 volatile uint32_t *const GPIOA_PUPDR = (volatile uint32_t*) (GPIOA_BASE + 0x0CUL);
@@ -36,6 +38,12 @@ volatile uint32_t *const TIM2_SR = (volatile uint32_t*) (TIM2_BASE + 0x10UL);
 volatile uint32_t *const TIM2_EGR = (volatile uint32_t*) (TIM2_BASE + 0x14UL);
 volatile uint32_t *const TIM2_DIER = (volatile uint32_t*) (TIM2_BASE + 0x0CUL);
 
+volatile uint32_t *const EXTI_IMR = (volatile uint32_t*) (EXTI_BASE + 0x00UL);
+volatile uint32_t *const EXTI_FTSR = (volatile uint32_t*) (EXTI_BASE + 0x0CUL);
+volatile uint32_t *const EXTI_PR = (volatile uint32_t*) (EXTI_BASE + 0x14UL);
+
+volatile uint32_t *const SYSCFG_EXTICR1 = (volatile uint32_t*) (SYSCFG_BASE + 0x08UL);
+
 // NVIC ISER0 (Interrupt Set-Enable Register) interrupts 0 - 31 bits
 // TIM2 is at 28 position
 volatile uint32_t *const NVIC_ISER0 = (volatile uint32_t*) 0xE000E100UL;
@@ -43,10 +51,10 @@ volatile uint32_t *const NVIC_ISER0 = (volatile uint32_t*) 0xE000E100UL;
 void delay(uint32_t ms);
 void led_on(void);
 void led_off(void);
-void startup_led_blink(void);
 void usart_send_char(const char ch);
 void usart_send_str(const char *str);
 void TIM2_IRQHandler(void);
+void EXTI0_IRQHandler(void);
 
 int main(void) {
 	// Enable Debugger in sleep/stop/standby modes not to brick the board
@@ -63,6 +71,9 @@ int main(void) {
 
 	// Enable TIM2 on APB1
 	*APB1_ENR |= (1 << 0);
+
+	// Enable SYSCFG
+	*APB2_ENR |= (1 << 14);
 
 	// SET PA0 Mode input (default 00) just clear bits to make sure
 	*GPIOA_MODER &= ~(3 << 0);
@@ -113,6 +124,15 @@ int main(void) {
 	// Enable counter (CEN counter enable)
 	*TIM2_CR1 |= (1 << 0);
 
+	// Setup Interrup for the button
+	// Setup EXTI0 to Port A (bits 0-3 should be clean)
+	*SYSCFG_EXTICR1 &= ~(0xF << 0);
+	// Setup EXTI0 to falling edge
+	*EXTI_IMR |= (1 << 0); // Allow line 0
+	*EXTI_FTSR |= (1 << 0);
+	// Enable interrupt EXI0 in NVIC (position 6)
+	*NVIC_ISER0 |= (1 << 6);
+
 	usart_send_str("Welcome to the STM32 World!\r\n");
 
 	// startup_led_blink();
@@ -133,42 +153,6 @@ void led_on(void) {
 
 void led_off(void) {
 	*GPIOC_BSRR = (1 << 13);
-}
-
-void startup_led_blink(void) {
-	usart_send_str("=============================\r\n");
-
-	// Now turn on bit with BSRR
-	// the LED is inverted on the board so add +16bits
-	led_on();
-	usart_send_str("LED ON\r\n");
-
-	// Just toggle the LED
-	// wait 1s, turn off, wait .5s turn off, wait .5s turn off
-	delay(1000);
-
-	led_off();
-	usart_send_str("LED OFF\r\n");
-	delay(500);
-
-	led_on();
-	usart_send_str("LED ON\r\n");
-	delay(500);
-
-	led_off();
-	usart_send_str("LED OFF\r\n");
-	delay(500);
-
-	led_on();
-	usart_send_str("LED ON\r\n");
-	delay(500);
-
-	led_off();
-	usart_send_str("LED OFF\r\n");
-	delay(500);
-
-	usart_send_str("=============================\r\n");
-	usart_send_str("Press button on the board to turn the LED ON\r\n");
 }
 
 void usart_send_char(const char ch) {
@@ -198,5 +182,31 @@ void TIM2_IRQHandler(void) {
 				state = 1;
 			}
 		}
+	}
+}
+
+void EXTI0_IRQHandler(void) {
+	if (*EXTI_PR & (1 << 0)) { // Check if interrupt happened on line 0
+		*EXTI_PR |= (1 << 0); // Rest flag 1 for reset
+
+		static int mode = 0;
+		mode++;
+
+		if (mode > 2)
+			mode = 0;
+
+		if (mode == 0) {
+			*TIM2_ARR = 500 - 1;
+			usart_send_str("Mode: Normal\r\n");
+		} else if (mode == 1) {
+			*TIM2_ARR = 100 - 1;
+			usart_send_str("Mode: Fast\r\n");
+		} else {
+			*TIM2_ARR = 2000 - 1;
+			usart_send_str("Mode: Slow\r\n");
+		}
+
+		// Update timer event
+		*TIM2_EGR |= (1 << 0);
 	}
 }
